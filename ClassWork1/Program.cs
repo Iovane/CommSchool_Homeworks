2d574f7a97a5dc08e1c213e8d23a5d61e0c35640
Iovane Chikovani <iovchikovani@tbcbank.ge>
2025-09-04 19:54:00 +0400

    add classwork project and finish homework

---
+++ ClassWork1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main()
+    {
+        /*int[] numbers = [15, 23, 12, 25, 100];
+        var minValue = numbers[0];
+        var maxValue = numbers[0];
+        
+        foreach (var number in numbers)
+        {
+            minValue = number < minValue ? number : minValue;
+            maxValue = number > maxValue ? number : maxValue;
+        } 
+        
+        Console.WriteLine($"Min value: {minValue}");
+        Console.WriteLine($"Max value: {maxValue}");*/
+        
+        Console.WriteLine("Enter a word: ");
+        var word = Console.ReadLine();
+        
+        var count = word!
+            .GroupBy(x => x)
+            .ToDictionary(x => x.Key, x => x.Count());
+        
+        Console.WriteLine("Frequencies:");
+        foreach (var item in count)
+        {
+            Console.WriteLine($"{item.Key} - {item.Value}");
+        }
+    }
+}
