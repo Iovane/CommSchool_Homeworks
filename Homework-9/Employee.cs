c43e11fd5a179886dfb787e91fa86be0c786cc22
Iovane <iovane.chikovani.1@gmail.com>
2025-09-14 21:50:39 +0400

    finish task 1

---
+++ Homework-9/Employee.cs
@@ -0,0 +1,70 @@
+﻿namespace Homework_9;
+
+public class Employee(string name, string lastName, int age, string occupation, List<int> hoursWorked, bool isNative)
+    : Company(isNative)
+{
+    private string Name { get; } = name;
+    private string LastName { get; } = lastName;
+    private int Age { get; } = age;
+    private string Occupation { get; } = occupation;
+    private List<int> HoursWorked { get; } = hoursWorked;
+
+    private readonly Dictionary<string, int> _hourlyRates = new()
+    {
+        { "Manager", 40 },
+        { "Developer", 30 },
+        { "Tester", 20 },
+        { "Other", 10 }
+    };
+    private const int OvertimeHourlyRate = 5;
+    private const int WeekendMultiplier = 2;
+    private const int StandardWorkHours = 8;
+
+    public void CalculateSalary()
+    {
+        var salaryInWeek = 0;
+
+        for (var i = 0; i < HoursWorked.Count; i++)
+        {
+            var hoursForDay = HoursWorked[i];
+            var isWeekend = i >= 5;
+
+            salaryInWeek += CalculateDailySalary(hoursForDay, Occupation, isWeekend);
+        }
+
+        CalculateTax(Occupation, GetHourlyRate(Occupation), salaryInWeek);
+    }
+
+    private int CalculateDailySalary(int hours, string occupation, bool isWeekend)
+    {
+        var hourlyRate = GetHourlyRate(occupation);
+        
+        if (isWeekend)
+        {
+            return hours * hourlyRate * WeekendMultiplier;
+        }
+
+        if (hours <= StandardWorkHours)
+        {
+            return hours * hourlyRate;
+        }
+
+        return (StandardWorkHours * hourlyRate) +
+               ((hours - StandardWorkHours) * OvertimeHourlyRate);
+    }
+    
+    private int GetHourlyRate(string occupation)
+    {
+        int hourlyRate;
+        try
+        {
+            hourlyRate = _hourlyRates[occupation];
+        }
+        catch (KeyNotFoundException ignored)
+        {
+            hourlyRate = _hourlyRates["Other"];
+        }
+
+        return hourlyRate;
+    }
+}
---
+++ Homework-9/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework_9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var employee = new Employee("John", "Doe", 25, "Developer", [8, 8, 8, 8, 8, 8, 0], true);
+        var employee1 = new Employee("Lika", "Doe", 23, "Manager", [8, 12, 8, 5, 8, 0, 0], false);
+        var employee2 = new Employee("007", "Doe", 18, "Tester", [8, 8, 8, 8, 8, 8, 0], true);
+        var employee3 = new Employee("Luka", "Doe", 70, "Other", [8, 8, 8, 8, 8, 8, 0], true);
+        var employee4 = new Employee("Davit", "Doe", 43, "Magari Devops", [12, 8, 8, 12, 8, 8, 12], false);
+        
+        employee.CalculateSalary();
+        employee1.CalculateSalary();
+        employee2.CalculateSalary();
+        employee3.CalculateSalary();
+        employee4.CalculateSalary();
+    }
+}
