d0e148bff60887035192565a13d920678575d8c4
Iovane Chikovani <iovchikovani@tbcbank.ge>
2025-09-21 22:40:39 +0400

    classwork 11

---
+++ Classwork-11/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Classwork_11;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number of lines: ");
+        var n = int.Parse(Console.ReadLine()!);
+
+        const string file = "/Users/iovchikovani/RiderProjects/CommSchool_Homeworks/Classwork-11/Files/file.txt";
+        File.WriteAllText(file, "");
+
+        for (var i = 0; i < n; i++)
+        {
+            Console.WriteLine($"line number {i + 1}:");
+            var text = Console.ReadLine();
+            File.AppendAllText(file, text + "\n");
+        }
+
+        Console.WriteLine("Input line to read");
+        var text1 = Console.ReadLine();
+        var lines = File.ReadAllLines(file);
+        Console.WriteLine(lines[int.Parse(text1) - 1]);
+    }
+}
